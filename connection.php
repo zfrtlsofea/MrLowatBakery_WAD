fdd55ef60dd29d45b080d250192080add87a1eb7
zfrtlsofea <sofea306@yahoo.com>
2025-01-02 17:41:14 +0800

    Update database name and set default timezone in connection file

--- connection.php
+++ connection.php
@@ -4,10 +4,14 @@
 $servername = "localhost";
 $username = "root";
 $password = ""; // Use your database password
-$dbname = "mrlowatbakerysystem";
+$dbname = "mrlowatbakery";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+date_default_timezone_set('Asia/Kuala_Lumpur');
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . mysqli_connect_error());
 }
